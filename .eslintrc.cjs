363e4daa685c7c04ce090835d6db944c0b384a72
Artur Iskhakov <artur.iskhakov@waveaccess.global>
2024-06-07 20:50:42 +0500

    Init nextjs app

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+const {
+    configure,
+    presets
+} = require("eslint-kit");
+
+module.exports = configure({
+    allowDebug: process.env.NODE_ENV !== "production",
+
+    presets: [
+        presets.imports(),
+        presets.node(),
+        presets.prettier(),
+        presets.typescript(),
+        presets.react(),
+        presets.nextJs()
+    ]
+});
